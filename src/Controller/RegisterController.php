2cf4de2d61b9b9c5e4f8c186358b51bd595c9f70
erguculu <erguculu@gmail.com>
2021-02-01 21:11:53 +0100

    created user entity

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,19 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegisterController extends AbstractController
 {
     /**
-     * @Route("/register", name="register")
+     * @Route("/inscription", name="register")
+     * @param Request $request
+     * @param EntityManagerInterface $em
+     * @param UserPasswordEncoderInterface $encoder
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder ): Response
     {
+        $user = new User();
+        $form =$this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData();
+
+            $password = $encoder->encodePassword($user, $user->getPassword());
+
+            $user->setPassword($password);
+
+            $em->persist($user);
+
+            $em->flush();
+
+
+        }
         return $this->render('register/index.html.twig', [
-            'controller_name' => 'RegisterController',
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,17 +4,57 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
+            ->add('Firstname', TextType::class, [
+                'label' => 'Prénom',
+                'constraints' => new Length([
+                    'min' =>2,
+                    "max" => 55,
+                ]),
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre prénom'
+                ]
+            ])
+            ->add('Lastname', TextType::class, [
+                'label' => 'Nom',
+                'constraints' => new Length([
+                    'min' =>2,
+                    "max" => 55,
+                ]),
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class,[
+                'label' => 'Email',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir votre adresse email'
+                ]
+            ])
+            ->add('password', RepeatedType::class,[
+                'type' => PasswordType::class,
+                'invalid_message' => 'Votre mot de passe et le confirmation doivent être identiques ',
+                'required' => true,
+                'label' => 'Mot de passe',
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation de mot de passe'],
+            ])
+            ->add('submit', SubmitType::class,[
+                'label' => 's\'inscrire'
+            ])
         ;
     }
 
