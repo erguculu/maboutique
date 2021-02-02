58a55f003f77ace8e6192900a0d7c1e2497e1a97
erguculu <erguculu@gmail.com>
2021-02-02 13:49:04 +0100

    add fonctionality for update password

--- src/Controller/AccountPasswordController.php
+++ src/Controller/AccountPasswordController.php
@@ -2,19 +2,49 @@
 
 namespace App\Controller;
 
+use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountPasswordController extends AbstractController
 {
     /**
-     * @Route("/account/password", name="account_password")
+     * @Route("/compte/modifier_mon-mot-de-passe", name="account_password")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @param EntityManagerInterface $em
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request,UserPasswordEncoderInterface $encoder, EntityManagerInterface $em): Response
     {
-        return $this->render('account_password/index.html.twig', [
-            'controller_name' => 'AccountPasswordController',
+        $notification = null;
+
+        $user = $this->getUser();
+        $form = $this->createForm(ChangePasswordType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $oldPassword = $form->get('old_password')->getData();
+
+            if($encoder->isPasswordValid($user, $oldPassword)){
+                $newPassword = $form->get('new_password')->getData();
+                $password = $encoder->encodePassword($user, $newPassword);
+                
+                $user->setPassword($password);
+                $em->flush();
+                $notification = "Votre mot de passe a bien été mis à jour";
+            }else{
+                $notification = "Votre mot de passe actuel n'est pas le bon";
+            }
+        }
+        return $this->render('account/password.html.twig',[
+            'form' => $form->createView(),
+            'notification' => $notification,
         ]);
     }
 }
