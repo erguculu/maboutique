7d054eb2e944d3c520c0fb692d8548cc453a5aaa
erguculu <erguculu@gmail.com>
2021-02-18 09:29:25 +0100

    Created order validate controller

--- src/Controller/StripeController.php
+++ src/Controller/StripeController.php
@@ -77,9 +77,12 @@ class StripeController extends AbstractController
                 $products_for_stripe,
             ],
             'mode' => 'payment',
-            'success_url' => $YOUR_DOMAIN . '/success.html',
-            'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+            'success_url' => $YOUR_DOMAIN . '/commande/merci/{CHECKOUT_SESSION_ID}',
+            'cancel_url' => $YOUR_DOMAIN . '/commande/erreur/{CHECKOUT_SESSION_ID}',
         ]);
+
+        $order->setStripeSessionId($checkout_session->id);
+
         $response = new  JsonResponse(['id' => $checkout_session->id]);
         return $response;
     }
